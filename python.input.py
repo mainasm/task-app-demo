c891b088a93cf3ca59603d74d2652cff63e258c3
jagermeistr <jeremy.mwangi@strathmore.edu>
2024-06-04 23:23:43 +0300

    work change

---
+++ python.input.py
@@ -0,0 +1,14 @@
+print("please input your name")
+first_name = input ("First Name: ")
+
+print("please input your name")
+last_name = input ("Last Name: ")
+
+print("please input your age")
+age = int(input ("Age: "))
+
+
+print("your name is: " + first_name + "," +last name)
+another way of handling output
+
+
